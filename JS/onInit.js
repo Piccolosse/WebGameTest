39aa557e64fd249272f9b7a1d49793c9ee30c691
Piccolosse <augustin.darre@outlook.com>
2018-11-21 18:06:23 +0100

    Add files via upload

---
+++ JS/onInit.js
@@ -0,0 +1,5 @@
+function onInit(){
+    //document.getElementById("likeImage").addEventListener('mousedown', function(){
+   //     window.gameInstance.SendMessage("SearchController", "ButtonSearch");
+  //  });
+}
